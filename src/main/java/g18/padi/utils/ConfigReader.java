591734feccf9fea5abc706aeccd0be003a6a6799
cacobaco <lcjmonizf09@gmail.com>
2024-03-23 14:56:31 +0000

    feat(config): improve exception handling

--- src/main/java/g18/padi/utils/ConfigReader.java
+++ src/main/java/g18/padi/utils/ConfigReader.java
@@ -1,8 +1,8 @@
 package g18.padi.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -17,11 +17,13 @@ public class ConfigReader {
     /**
      * Private constructor of ConfigReader class.
      * Initializes the configuration settings by reading from the 'project.config' file.
-     *
-     * @throws IOException if an I/O error occurs while reading the configuration file
      */
-    private ConfigReader() throws IOException {
-        config = readConfig("project.config");
+    private ConfigReader() {
+        try {
+            config = readConfig("project.config");
+        } catch (IOException e) {
+            System.out.println("Error reading configuration file: " + e.getMessage());
+        }
     }
 
     /**
@@ -29,7 +31,7 @@ public class ConfigReader {
      *
      * @return the singleton instance
      */
-    public static ConfigReader getInstance() throws IOException {
+    public static ConfigReader getInstance() {
         if (instance == null) {
             instance = new ConfigReader();
         }
@@ -39,8 +41,8 @@ public class ConfigReader {
     /**
      * Reads configuration settings from a JSON file.
      *
-     * @param filePath   the path to the JSON configuration file
-     * @return           a JsonNode containing the configuration settings
+     * @param filePath the path to the JSON configuration file
+     * @return a JsonNode containing the configuration settings
      * @throws IOException if an I/O error occurs while reading the file
      */
     public JsonNode readConfig(String filePath) throws IOException {
@@ -51,7 +53,7 @@ public class ConfigReader {
     /**
      * Retrieves the number of columns for the client from the configuration settings.
      *
-     * @return           the number of client columns
+     * @return the number of client columns
      */
     public int getClientCols() {
         return config.get("client").get("cols_number").asInt();
@@ -60,7 +62,7 @@ public class ConfigReader {
     /**
      * Retrieves the number of rows for the client from the configuration settings.
      *
-     * @return           the number of client rows
+     * @return the number of client rows
      */
     public int getClientRows() {
         return config.get("client").get("rows_number").asInt();
@@ -69,7 +71,7 @@ public class ConfigReader {
     /**
      * Retrieves the maximum number of connections allowed for the server from the configuration settings.
      *
-     * @return           the maximum number of server connections
+     * @return the maximum number of server connections
      */
     public int getServerMaxConnections() {
         return config.get("server").get("max_conn").asInt();
@@ -78,7 +80,7 @@ public class ConfigReader {
     /**
      * Retrieves the maximum processing capacity allowed for the server from the configuration settings.
      *
-     * @return           the maximum processing capacity of the server
+     * @return the maximum processing capacity of the server
      */
     public int getServerMaxProcessingCapacity() {
         return config.get("server").get("max_processing_capacity").asInt();
@@ -87,7 +89,7 @@ public class ConfigReader {
     /**
      * Retrieves the filename for the load info file from the configuration settings.
      *
-     * @return           the filename for the load info file
+     * @return the filename for the load info file
      */
     public String getLoadInfoFile() {
         return config.get("load_info_file").asText();
@@ -96,7 +98,7 @@ public class ConfigReader {
     /**
      * Retrieves the total number of servers from the configuration settings.
      *
-     * @return           the total number of servers
+     * @return the total number of servers
      */
     public int[] getServers() {
         JsonNode portsNode = config.get("server").get("ports");
