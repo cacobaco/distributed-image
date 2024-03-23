8268cf5d21474a1627bdc81ff0443edd9e25f158
pingucommie <brunoaguiar03@gmail.com>
2024-03-23 10:58:10 +0000

    Add Producer Consumer to the server to handle multiple clients

---
+++ src/main/java/g18/padi/server/BufferedImageSplit.java
@@ -0,0 +1,54 @@
+package g18.padi.server;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * The class BufferedImageSplit represents an image split and its corresponding position in the final image. The objects
+ * of this class will be put in the buffer for the consuming threads and in the resulting buffer.
+ */
+public class BufferedImageSplit {
+
+    private final int i;
+    private final int j;
+    private final BufferedImage bufferedImage;
+
+    /**
+     * Constructs a BufferedImageSplit by specifying the image split and its position on the final image.
+     *
+     * @param bufferedImage the BufferedImage containing the image split
+     * @param i             the row index of the image split in the final image
+     * @param j             the column index of the image split in the final image
+     */
+    public BufferedImageSplit ( BufferedImage bufferedImage , int i , int j ) {
+        this.bufferedImage = bufferedImage;
+        this.i = i;
+        this.j = j;
+    }
+
+    /**
+     * Gets the BufferedImage containing the image split
+     *
+     * @return the BufferedImage containing the image split
+     */
+    public BufferedImage getBufferedImage ( ) {
+        return bufferedImage;
+    }
+
+    /**
+     * Get the row index of the image split in the final image
+     *
+     * @return the row index of the image split in the final image
+     */
+    public int getI ( ) {
+        return i;
+    }
+
+    /**
+     * Get the column index of the image split in the final image
+     *
+     * @return the column index of the image split in the final image
+     */
+    public int getJ ( ) {
+        return j;
+    }
+}
---
+++ src/main/java/g18/padi/server/ConsumerThread.java
@@ -0,0 +1,56 @@
+package g18.padi.server;
+import g18.padi.utils.ImageTransformer;
+
+import java.awt.image.BufferedImage;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The ConsumerThread represents a consumer thread, which is responsible for removing the red component.
+ */
+public class ConsumerThread extends Thread {
+
+    private final BlockingQueue < BufferedImageSplit > buffer;
+    private final BlockingQueue < BufferedImageSplit > resultingBuffer;
+
+    private final int nTotalImages;
+
+    /**
+     * Constructs a consumer thread responsible for retrieving image splits from the buffer, processing them, and
+     * putting the result in the resulting buffer.
+     *
+     * @param buffer          the shared buffer to which consumer threads will retrieve images splits
+     * @param resultingBuffer the buffer to which the consumer threads will put the processes image splits
+     * @param nTotalImages    the total number of images to process (given by the number of rows times the number of
+     *                        columns)
+     */
+    public ConsumerThread (BlockingQueue<BufferedImageSplit> buffer , BlockingQueue<BufferedImageSplit> resultingBuffer , int nTotalImages ) {
+        this.buffer = buffer;
+        this.resultingBuffer = resultingBuffer;
+        this.nTotalImages = nTotalImages;
+    }
+
+    @Override
+    public void run ( ) {
+        boolean stop = false;
+        while ( ! stop ) {
+            try {
+                BufferedImageSplit originalImage = buffer.poll ( 100 , TimeUnit.MILLISECONDS );
+                if ( originalImage == null ) {
+                    continue;
+                }
+                BufferedImage bufferedImage = ImageTransformer.removeReds ( originalImage.getBufferedImage ( ) );
+                BufferedImageSplit bufferedImageSplit = new BufferedImageSplit ( bufferedImage , originalImage.getI ( ) , originalImage.getJ ( ) );
+                resultingBuffer.add ( bufferedImageSplit );
+            } catch ( InterruptedException e ) {
+                e.printStackTrace ( );
+            } finally {
+                if ( this.nTotalImages == resultingBuffer.size ( ) ) {
+                    stop = true;
+                }
+            }
+        }
+    }
+
+
+}
